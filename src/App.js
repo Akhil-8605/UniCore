7927041670b4bd16ea1543e62115c28667bb767b
Akhil <akhileshadam186@gmail.com>
2025-01-10 13:50:21 +0530

    Convert to HashRouter and prepare for GitHub Pages deployment

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Homepage from "./Pages/homepage";
 import AuthPage from "./Components/AuthPage";
 import ContactPage from "./Pages/contactpage";
 
-import LibraryPage from "./Pages/library-management/LibraryPage";
+import LibraryPage from "./Pages/library-management/LibraryPage"
 import AdminLibrary from "./Pages/library-management/admin-library";
 import AcademicCalendar from "./Pages/AcademicCalendarPage";
 
@@ -15,12 +15,12 @@ function App() {
         <Routes>
           <Route path="/" element={<Homepage />} />
           <Route path="/login" element={<AuthPage />} />
-          <Route path="/contact" element={<ContactPage />} />
+          <Route path="/contact" element={<ContactPage/>}></Route>
           
-          <Route path="/admin-library" element={<AdminLibrary />} />
-          <Route path="/library" element={<LibraryPage />} />
-          
-          <Route path="/academic-calendar" element={<AcademicCalendar />} />
+          <Route path="/admin-library" element={<AdminLibrary/>}></Route>
+          <Route path="/library" element={<LibraryPage/>}></Route>
+
+          <Route path="/acedamic-calendar" element={<AcademicCalendar/>}></Route>
         </Routes>
       </Router>
     </>
