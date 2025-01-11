e793fdedb4a4ec84ac19e2f7a0185904158abed5
Akhil <akhileshadam186@gmail.com>
2025-01-11 18:52:19 +0530

    Convert to HashRouter and prepare for GitHub Pages deployment

--- src/App.js
+++ src/App.js
@@ -3,24 +3,22 @@ import "./App.css";
 import Homepage from "./Pages/homepage";
 import AuthPage from "./Components/AuthPage";
 import ContactPage from "./Pages/contactpage";
-
-import LibraryPage from "./Pages/library-management/LibraryPage"
+import LibraryPage from "./Pages/library-management/LibraryPage";
 import AdminLibrary from "./Pages/library-management/admin-library";
 import AcademicCalendar from "./Pages/AcademicCalendarPage";
 
 function App() {
   return (
     <>
+      {/* Updated Router with correct basename */}
       <Router basename="/unicore-demo">
         <Routes>
           <Route path="/" element={<Homepage />} />
           <Route path="/login" element={<AuthPage />} />
-          <Route path="/contact" element={<ContactPage/>}></Route>
-          
-          <Route path="/admin-library" element={<AdminLibrary/>}></Route>
-          <Route path="/library" element={<LibraryPage/>}></Route>
-
-          <Route path="/acedamic-calendar" element={<AcademicCalendar/>}></Route>
+          <Route path="/contact" element={<ContactPage />} />
+          <Route path="/admin-library" element={<AdminLibrary />} />
+          <Route path="/library" element={<LibraryPage />} />
+          <Route path="/academic-calendar" element={<AcademicCalendar />} />
         </Routes>
       </Router>
     </>
